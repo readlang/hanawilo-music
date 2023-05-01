db1f7687c637f2ff189edb018608b1097c615faf
Read Langworthy <rlangworthy@gmail.com>
2023-05-01 16:28:16 -0700

    before deploying

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const logger = require('./middlewares/logger')
 const errorHandler = require('./middlewares/error')
 const cookieParser = require('cookie-parser')
 const fileupload = require('express-fileupload')
+const mongoSanitize = require('express-mongo-sanitize')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const hpp = require('hpp')
+const xss = require('xss-clean')
 
 // this imports the various routes of the server
 const artist = require('./routes/artist')
@@ -22,14 +27,27 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: '*'
+    origin: '*' // this is wildcard string, allowing any site
 }))
 
 // middleware to use for all routes go here:
 app.use(cookieParser())
 app.use(fileupload()) // allows grabbing data out of uploaded files
+
+app.use(mongoSanitize())
+app.use(xss())
+
+app.use(hpp())
+app.use(helmet())
 app.use(logger)
 app.use(errorHandler)
+
+// this is a pretty relaxed rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // max api calls in window
+})
+app.use(limiter)
 
 // these are defining the routes
 app.use('/artist', artist)
